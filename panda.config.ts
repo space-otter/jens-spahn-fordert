67272deef17d8c882a0233753713aff8549b0bf4
space-otter <patrick@prismaticweb.io>
2025-04-15 12:06:59 +0200

    add base style tokens and fonts

--- panda.config.ts
+++ panda.config.ts
@@ -12,7 +12,28 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        colors: {
+          paper: { value: "#1B4194" },
+          pen: { value: "#1B0E0B" },
+          accent: { value: "#FFFFFF" },
+        },
+        fonts: {
+          inter: { value: "'Inter Variable', sans-serif" },
+          boska: { value: "Boska-Variable, sans-serif" },
+        },
+        fontSizes: {
+          sm: { value: "clamp(0.7502rem, 0.7234rem + 0.134vw, 0.844rem)" },
+          base: { value: "clamp(1rem, 0.9643rem + 0.1786vw, 1.125rem)" },
+          md: { value: "clamp(1.5625rem, 1.4378rem + 0.6236vw, 1.999rem)" },
+          lg: { value: "clamp(1.9531rem, 1.1497rem + 4.0174vw, 4.7653rem)"}
+        },
+        spacing: {
+          contentArea: { value: "min(100%, 90rem)" },
+        }
+      }
+    },
   },
 
   // The output directory for your css system
